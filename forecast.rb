fb2dd9566bc5d1459ad0f7a3fbf34b249df3bc61
Andrey Timakhovich <stewart96.at@gmail.com>
2022-06-16 14:04:16 +0300

    get forecast from legacy.meteoservice.ru

---
+++ forecast.rb
@@ -0,0 +1,49 @@
+# подключаем либы для работы с сетью и XML
+require "net/http"
+require "uri"
+require "rexml/document"
+
+# Массив строк для облачности, описанный на сайте метеосервиса
+CLOUDINESS = %w(Ясно Малооблачно Облачно Пасмурно).freeze
+
+# Сформируем адрес запроса с сайта метеосервиса
+#
+# 25346 - Воложин
+uri = URI.parse('https://xml.meteoservice.ru/export/gismeteo/point/25346.xml')
+
+# Отправляем HTTP-запрос по указанному адресу и записываем ответ в переменную
+# response.
+response = Net::HTTP.get_response(uri)
+
+# Из тела ответа (body) формируем XML документ с помощью REXML парсера
+doc = REXML::Document.new(response.body)
+
+# Получаем имя города из XML, город лежит в ноде REPORT/TOWN
+city_name = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
+
+# Достаем первый XML тэг из списка <FORECAST> внутри <TOWN> — прогноз на
+# ближайшее время со всей нужной нам информацией.
+forecast = doc.root.elements['REPORT/TOWN/FORECAST']
+
+# Записываем минимальное и максимальное значение температуры из аттрибутов min
+# и max вложенного в элемент FORECAST тэга TEMPERATURE
+min_temp = forecast.elements['TEMPERATURE'].attributes['min']
+max_temp = forecast.elements['TEMPERATURE'].attributes['max']
+
+# Записываем максимальную скорость ветра из атриубута max тэга WIND
+max_wind = forecast.elements['WIND'].attributes['max']
+
+# Достаем из тега PHENOMENA атрибут cloudiness и по его значению находим строку
+# с обозначением облачности из массива CLOUDINESS
+clouds_index = forecast.elements['PHENOMENA'].attributes['cloudiness'].to_i
+clouds = CLOUDINESS[clouds_index]
+
+# Выводим всю информацию на экран
+puts city_name
+puts "Температура — от #{min_temp} до #{max_temp} С"
+puts "Ветер #{max_wind} м/с"
+puts clouds
+
+puts
+puts "Предоставлено Meteoservice.ru"
+puts "https://www.meteoservice.ru/"
